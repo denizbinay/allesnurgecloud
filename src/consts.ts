c2addcdf2d7f2e1402a10bee5612c1523cde831a
denizbinay <deniz@bombig.net>
2023-04-05 23:39:52 +0200

    init repo

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "allesnurgecloud";
+export const SITE_DESCRIPTION = "l3375p33k 4 l1uf3";
